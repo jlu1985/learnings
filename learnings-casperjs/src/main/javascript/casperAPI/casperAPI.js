21ba388a5ac446a631aac2953f0a89847ba9c927
Jia Lu <jlu1985@gmail.com>
2015-06-26 18:33:21 -0400

    temp

--- learnings-casperjs/src/main/javascript/casperAPI/casperAPI.js
+++ learnings-casperjs/src/main/javascript/casperAPI/casperAPI.js
@@ -1,26 +1,56 @@
-//getting an insance of casper
-//var casper = require('casper').create();
-
-//alternative
-//var casper2 = new require('casper').Casper();
-
 var casper = require('casper').create({
-    logLevel: "debug",
     verbose: true
-
 });
 
-casper.start("http://casper.local", function () {
-    this.echo(this.getHTML());
+function printHtml(_casper) {
+    _casper.then(function () {
+        _casper.echo("Current url: " + _casper.getCurrentUrl());
+        _casper.debugHTML();
+    });
+
+}
+
+function gotoFormFromHome(_casper){
+    _casper.thenClick("#aForm",function () {
+        this.echo("clicked form");
+        printHtml(_casper);
+    });
+    
+    printHtml(_casper);
+}
+
+casper.start("http://casper.local", function (response) {
+    this.echo("Opened http://casper.local " + this.getCurrentUrl());
+   require('utils').dump(response);
 });
 
-casper.then(function () {
-    casper.click('#oneImportantLink');
-
+casper.thenClick('#oneImportantLink',function () {
+    this.echo("clicked on a link that has an ID: #oneImportantLink");
+    printHtml(casper);
 });
 
-casper.then(function () {
-    this.echo(this.getHTML());
+casper.thenClick("#homepage",function () {
+    this.echo("clicked home");
+    printHtml(casper);
+});
+
+gotoFormFromHome(casper);
+
+casper.then(function(){
+    this.echo("We are going to fill out the form by using fill()");
+    this.fill("#contactForm",{
+        "name": "Casper Rocks",
+        "email": "oneLongEmail@a.com"
+    }, true);
+});
+
+casper.then(function(){
+    
+    this.evaluate(function(){
+        __utils__.echo("calling '__utils__.echo' in evaluate");
+    });
+    
+    printHtml(casper);
 });
 
 casper.run();
