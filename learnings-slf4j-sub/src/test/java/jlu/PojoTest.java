7874132e2d1a9d62de6e41297a896c1adf7aa85c
Jia Lu <jlu1985@gmail.com>
2015-09-01 20:55:42 -0400

    left out test

---
+++ learnings-slf4j-sub/src/test/java/jlu/PojoTest.java
@@ -0,0 +1,14 @@
+package jlu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PojoTest {
+
+    @Test
+    public void test() {
+        new Pojo().doWork();
+    }
+
+}
