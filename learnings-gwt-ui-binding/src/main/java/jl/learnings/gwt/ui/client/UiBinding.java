198da6a4474ac909c591a802873000f660c60f22
Jia Lu <jlu1985@gmail.com>
2016-09-05 18:07:41 -0400

    gwt ui binding skeleton

---
+++ learnings-gwt-ui-binding/src/main/java/jl/learnings/gwt/ui/client/UiBinding.java
@@ -0,0 +1,13 @@
+package jl.learnings.gwt.ui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class UiBinding implements EntryPoint{
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
